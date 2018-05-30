8560410d66c804bc69b4c2211d5e8112b8df2202
sergeyfront <ser.mazur501@gmail.com>
2018-05-30 18:28:49 +0300

    create proj

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DynamicFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
